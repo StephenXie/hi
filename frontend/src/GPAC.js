e93746be1bbad4147481dc2e5c536cceb1a04ffa
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-10-16 19:13:55 -0700

    Add background gfx for todo

--- frontend/src/GPAC.js
+++ frontend/src/GPAC.js
@@ -1,47 +1,78 @@
-import { useState  } from "react";
+import { useState } from "react";
 import { withRouter } from "react-router-dom";
+import { AcademicCapIcon } from "@heroicons/react/solid";
 import Header from "./components/Header";
 import Particle from "./components/Particle";
-import Button from "./components/Button"
+import Button from "./components/Button";
 import GPA from "./components/GPA";
 import Classes from "./components/Classes";
 import Class from "./components/Class";
 import Highlight from "./components/Highlight";
-import Tilt from 'react-tilt'
+import Tilt from "react-tilt";
 const GPAC = () => {
-    const [classes, setClasses] = useState([{id:0, name: "", grade: "A", weight:"R", credit: "1"}]); 
-    const [numClass, setNumClass] = useState(1);
-    const addClass = () => {
-      setClasses([...classes, {id: numClass, name: "", grade: "A", weight:"R", credit: "1"}])
-      setNumClass(numClass+1);
-    }
-    const deleteClass = (id) =>{
-      console.log(classes)
-      setClasses(classes.filter((myclass) => myclass.id!==id))
-      
-    }
-    const setData = (id, name, grade, weight, credit) =>{
-      setClasses(classes.map((myclass) => ( myclass.id==id ? {...myclass, name: name, grade: grade, weight: weight, credit: credit} : myclass)))
-    }
-    return(
+  const [classes, setClasses] = useState([
+    { id: 0, name: "", grade: "A", weight: "R", credit: "1" },
+  ]);
+  const [numClass, setNumClass] = useState(1);
+  const addClass = () => {
+    setClasses([
+      ...classes,
+      { id: numClass, name: "", grade: "A", weight: "R", credit: "1" },
+    ]);
+    setNumClass(numClass + 1);
+  };
+  const deleteClass = (id) => {
+    console.log(classes);
+    setClasses(classes.filter((myclass) => myclass.id !== id));
+  };
+  const setData = (id, name, grade, weight, credit) => {
+    setClasses(
+      classes.map((myclass) =>
+        myclass.id == id
+          ? {
+              ...myclass,
+              name: name,
+              grade: grade,
+              weight: weight,
+              credit: credit,
+            }
+          : myclass
+      )
+    );
+  };
+  return (
     <div className="my-container flex-1 bg-white ">
       <Particle />
       <Header
-      className=""
+        className=""
         imgLink="https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=125&w=210"
         descriptions="Enter your grades, what types of classes you are taking, course credits(optional) and calculates your weighted and unweighted GPA."
-      button={<Tilt className="Tilt" options={{ max : 15 }} ><Button text={<div className="flex flex-row space-x-2"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path d="M12 14l9-5-9-5-9 5 9 5z" />
-      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-    </svg><span>Add Class</span></div>} color="green" onClick={addClass} className="relative"/></Tilt>}
+        button={
+          <div className="flex flex-row space-x-3">
+            <Tilt className="Tilt" options={{ max: 30 }}>
+              <Button
+                text={
+                  <div className="flex flex-row space-x-2">
+                    <AcademicCapIcon className="h-7 w-7 my-auto" />
+                    <span className="my-auto">Add Class</span>
+                  </div>
+                }
+                color="green"
+                onClick={addClass}
+                className="relative"
+              />
+            </Tilt>
+          </div>
+        }
       >
-        <div className="animate__animated animate__flip animate__faster"><Highlight color="green" text="GPA" r={600}/> Calculator</div> 
+        <div className="animate__animated animate__flip animate__fast">
+          <Highlight color="green" text="GPA" r={600} /> Calculator
+        </div>
       </Header>
-      <GPA key={classes} classes={classes}/>
-      <Classes classes={classes} deleteClass={deleteClass} setData={setData}/>
+      <GPA key={classes} classes={classes} />
+      <Classes classes={classes} deleteClass={deleteClass} setData={setData} />
     </div>
-    )
-}
+  );
+};
 
-export default withRouter(GPAC)
+export default withRouter(GPAC);
--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -68,6 +68,22 @@ function Todo() {
   };
   return (
     <div className="my-container flex-1">
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
+        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="128" cy="108" r="98" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
       <Particle />
       <Header
         title="Todo List"
